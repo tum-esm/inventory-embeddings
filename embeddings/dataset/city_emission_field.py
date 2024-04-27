cc3be8ab36ea3d4585086b309738d06f4709d9ee
must1d <mustafe.dobra@tum.de>
2024-04-27 19:38:11 +0000

    Adds Robust Scaling Factor

--- embeddings/dataset/city_emission_field.py
+++ embeddings/dataset/city_emission_field.py
@@ -7,6 +7,8 @@ from embeddings.common.gnfr_sector import NUM_GNFR_SECTORS
 
 
 class CityEmissionField:
+    ROBUST_SCALING_FACTOR = 1 / 2_500_000  # determined using 95th percentile
+
     def __init__(self, city_data: pl.DataFrame) -> None:
         """
         An emission field have dimension SxWxH.
@@ -26,7 +28,7 @@ class CityEmissionField:
         for p in city_data.iter_rows(named=True):
             self.lat_lon_array[(p["x"], p["y"])] = [p["lat"], p["lon"]]
             for i, co_2ff_sector in enumerate(p["co2_ff"].split(",")):
-                self.co2_ff_field[i, p["x"], p["y"]] = float(co_2ff_sector) / 1_000
+                self.co2_ff_field[i, p["x"], p["y"]] = float(co_2ff_sector) * self.ROBUST_SCALING_FACTOR
 
     @property
     def width(self) -> int:
