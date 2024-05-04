4d36bb766b49973db73088105ad0f19251d678fb
must1d <mustafe.dobra@tum.de>
2024-05-04 14:19:22 +0000

    Implements Evaluation Script for Gaussian Measurements

--- embeddings/evaluation/evaluate.py
+++ embeddings/evaluation/evaluate.py
@@ -1,32 +1,37 @@
-import random
-
-import torch
+import numpy as np
+from alive_progress import alive_bar
 from matplotlib import pyplot as plt
 
 from embeddings.common.paths import PlotPaths
 from embeddings.dataset.tno_dataset_collection import TnoDatasetCollection
 from embeddings.evaluation.compressed_sensing_experiment import CompressedSensingExperiment
 from embeddings.evaluation.inverse_problems_solver import GenerativeModelSolver
-from embeddings.plotting.city_emission_field_plot import plot_emission_field_tensor
 
 if __name__ == "__main__":
     dataset = TnoDatasetCollection(deterministic=True).validation_data
+    dataset.disable_temporal_transforms()
 
-    num_measurements = 250
+    measurements = [10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 12500]
+    reconstruction_errors = []
 
     solver = GenerativeModelSolver()
-    experiment = CompressedSensingExperiment(num_measurements=250)
 
-    x = dataset[random.randint(0, len(dataset) - 1)]
-    x_rec = experiment.solve_random_inverse_problem(x=x, solver=solver)
+    with alive_bar(5 * len(measurements) * len(dataset)) as bar:
+        for num_measurements in measurements:
+            mse_losses = []
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 10))
+            for x in dataset:
+                for _ in range(5):
+                    experiment = CompressedSensingExperiment(num_measurements=num_measurements)
+                    x_rec = experiment.solve_random_inverse_problem(x=x, solver=solver)
+                    mse = np.square(np.subtract(x, x_rec)).mean()
+                    mse_losses.append(mse)
+                    bar()
 
-    vmax = 1.1 * float(torch.max(x))
+            reconstruction_errors.append(np.mean(np.array(mse_losses)))
 
-    plot_emission_field_tensor(emission_field=x, ax=ax1, vmax=vmax)
-    ax1.title.set_text("Original Emission Field")
-    plot_emission_field_tensor(emission_field=x_rec, ax=ax2, vmax=vmax)
-    ax2.title.set_text(f"Reconstructed Emission Field\nWith {num_measurements} measurements")
+    print(reconstruction_errors)
 
-    plt.savefig(PlotPaths.PLOTS / "inverse_problem.png")
+    plt.plot(measurements, reconstruction_errors)
+    plt.xscale("log")
+    plt.savefig(PlotPaths.PLOTS / "measurements.png")
