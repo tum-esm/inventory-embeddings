33560ae0163b2bdb50551a6d804dd34470e84c3d
must1d <mustafe.dobra@tum.de>
2024-05-27 18:15:40 +0000

    Adds SSIM to Evaluation

--- embeddings/evaluation/evaluate.py
+++ embeddings/evaluation/evaluate.py
@@ -8,6 +8,7 @@ from embeddings.common.paths import ModelPaths
 from embeddings.dataset.tno_dataset_collection import TnoDatasetCollection
 from embeddings.evaluation.compressed_sensing_experiment import generate_random_inverse_problem, solve_inverse_problem
 from embeddings.evaluation.inverse_problems_solver import GenerativeModelSolver
+from embeddings.models.common.metrics import ssim
 
 NUM_MEASUREMENTS = [50, 100, 250, 500, 1000, 2500, 5000, 10000, 12500]
 NUM_EXPERIMENTS = 5
@@ -25,7 +26,7 @@ def evaluate() -> None:
 
     for run_index in tqdm(range(NUM_EXPERIMENTS), desc="Run"):
         evaluation_csv = CsvWriter(path=ModelPaths.VAE_LATEST_EVALUATION / f"evaluation_{run_index}.csv")
-        evaluation_csv.write_header("Measurements", "MSE")
+        evaluation_csv.write_header("Measurements", "MSE", "SSIM")
 
         with tqdm(total=len(NUM_MEASUREMENTS) * len(dataset), leave=False, desc="Compressed Sensing") as bar:
             for num_measurements in NUM_MEASUREMENTS:
@@ -33,5 +34,6 @@ def evaluate() -> None:
                     inverse_problem = generate_random_inverse_problem(x=x, num_measurements=num_measurements)
                     x_rec = solve_inverse_problem(solver=generative_model_solver, inverse_problem=inverse_problem)
                     mse = float(np.square(np.subtract(x, x_rec)).mean())
-                    evaluation_csv.write_row(num_measurements, mse)
+                    s = float(ssim(x=x, x_hat=x_rec))
+                    evaluation_csv.write_row(num_measurements, mse, s)
                     bar.update()
