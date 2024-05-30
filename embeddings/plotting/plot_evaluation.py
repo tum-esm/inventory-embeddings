af612a2a1594fbc43083ef92e159a30e117e21bd
must1d <mustafe.dobra@tum.de>
2024-05-30 09:28:06 +0000

    Improve Evaluation Plot

--- embeddings/plotting/plot_evaluation.py
+++ embeddings/plotting/plot_evaluation.py
@@ -4,28 +4,45 @@ import polars as pl
 
 from embeddings.common.paths import ModelPaths
 
+
+def _get_mean_and_confidence_tuples(metric: str) -> dict[int, tuple[float, float]]:
+    values_dict = {}
+    for num_measurements, group in evaluation.group_by(["Measurements"]):
+        values = group.get_column(metric).to_numpy()
+        mean = np.mean(values)
+        std_error = np.std(values, ddof=1) / np.sqrt(len(values))
+        confidence_interval = 1.96 * std_error  # 95% confidence interval
+        values_dict[num_measurements[0]] = (mean, confidence_interval)
+        values_dict = dict(sorted(values_dict.items()))
+    return values_dict
+
+
 if __name__ == "__main__":
     data_frames = [pl.read_csv(evaluation_csv) for evaluation_csv in ModelPaths.VAE_LATEST_EVALUATION.iterdir()]
     evaluation = pl.concat(data_frames)
 
-    mse_per_measurements = {}
+    mse_values = _get_mean_and_confidence_tuples(metric="MSE")
+    ssim_values = _get_mean_and_confidence_tuples(metric="SSIM")
 
-    for num_measurements, group in evaluation.group_by(["Measurements"]):
-        mse_values = group.get_column("MSE").to_numpy()
-        mean_mse = np.mean(mse_values)
-        std_error = np.std(mse_values, ddof=1) / np.sqrt(len(mse_values))
-        confidence_interval = 1.96 * std_error  # 95% confidence interval
-        mse_per_measurements[num_measurements[0]] = (mean_mse, confidence_interval)
+    measurements = list(mse_values.keys())
 
-    mse_per_measurements_sorted = dict(sorted(mse_per_measurements.items()))
+    mean_mse = [value[0] for value in mse_values.values()]
+    confidence_mse = [value[1] / 2 for value in mse_values.values()]
 
-    measurements = list(mse_per_measurements_sorted.keys())
-    mean_mse = [value[0] for value in mse_per_measurements_sorted.values()]
-    confidence_mse = [value[1] / 2 for value in mse_per_measurements_sorted.values()]
+    mean_ssim = [value[0] for value in ssim_values.values()]
+    confidence_ssim = [value[1] / 2 for value in ssim_values.values()]
 
-    plt.errorbar(x=measurements, y=mean_mse, yerr=confidence_mse)
-    plt.xscale("log")
-    plt.xticks(ticks=measurements, labels=measurements, rotation=90)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25, 10))
+
+    ax1.set_xscale("log")
+    ax1.set_xticks(ticks=measurements, labels=[str(m) for m in measurements], rotation=90)
+    ax1.set_xlim([measurements[0], measurements[-1]])
+    ax1.errorbar(x=measurements, y=mean_mse, yerr=confidence_mse)
+
+    ax2.set_xscale("log")
+    ax2.set_xticks(ticks=measurements, labels=[str(m) for m in measurements], rotation=90)
+    ax2.set_xlim([measurements[0], measurements[-1]])
+    ax2.errorbar(x=measurements, y=mean_ssim, yerr=confidence_ssim)
 
     ModelPaths.VAE_LATEST_PLOTS.mkdir(exist_ok=True)
     plt.savefig(ModelPaths.VAE_LATEST_PLOTS / "evaluation.png")
