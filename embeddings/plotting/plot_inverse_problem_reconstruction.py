368cb1eaf2527a28cea76ad96a409f103979c309
must1d <mustafe.dobra@tum.de>
2024-06-03 17:28:46 +0000

    Adds Different Solvers to Reconstruction Script

--- embeddings/plotting/plot_inverse_problem_reconstruction.py
+++ embeddings/plotting/plot_inverse_problem_reconstruction.py
@@ -7,7 +7,7 @@ from embeddings.common.log import logger
 from embeddings.common.paths import ModelPaths
 from embeddings.dataset.tno_dataset_collection import TnoDatasetCollection
 from embeddings.evaluation.compressed_sensing_experiment import generate_random_inverse_problem, solve_inverse_problem
-from embeddings.evaluation.inverse_problems_solver import GenerativeModelSolver
+from embeddings.evaluation.inverse_problems_solver import DwtLassoSolver, GenerativeModelSolver, LassoSolver
 from embeddings.models.common.metrics import mse, ssim
 from embeddings.plotting.city_emission_field_plot import plot_emission_field_tensor
 
@@ -17,23 +17,29 @@ if __name__ == "__main__":
 
     num_measurements = 500
 
-    solver = GenerativeModelSolver(plot_loss=True, log_info=True)
+    solvers = {
+        "Generative AI": GenerativeModelSolver(plot_loss=True, log_info=True),
+        "Lasso": LassoSolver(),
+        "Lasso + DWT": DwtLassoSolver(),
+    }
 
     x = dataset[random.randint(0, len(dataset) - 1)]
     inverse_problem = generate_random_inverse_problem(x=x, num_measurements=num_measurements)
-    x_rec = solve_inverse_problem(solver=solver, inverse_problem=inverse_problem)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 10))
-
+    fig, axes = plt.subplots(1, len(solvers) + 1, figsize=(5 + 5 * len(solvers), 8))
     vmax = 1.1 * float(torch.max(x))
+    plot_emission_field_tensor(emission_field=x, ax=axes[0], vmax=vmax)
+    axes[0].title.set_text("Original Emission Field")
 
-    plot_emission_field_tensor(emission_field=x, ax=ax1, vmax=vmax)
-    ax1.title.set_text("Original Emission Field")
-    plot_emission_field_tensor(emission_field=x_rec, ax=ax2, vmax=vmax)
-    ax2.title.set_text(f"Reconstructed Emission Field\nWith {num_measurements} measurements")
+    for index, (solver_name, solver) in enumerate(solvers.items()):
+        x_rec = solve_inverse_problem(solver=solver, inverse_problem=inverse_problem)
 
-    logger.info(f"MSE: {mse(x=x, x_hat=x_rec)}")
-    logger.info(f"SSIM: {ssim(x=x, x_hat=x_rec)}")
+        ax = axes[index + 1]
+        plot_emission_field_tensor(emission_field=x_rec, ax=ax, vmax=vmax)
+        ax.title.set_text(f"Reconstructed Emission Field\nWith {num_measurements} measurements\n{solver_name}")
+
+        logger.info(f"MSE {solver_name}: {mse(x=x, x_hat=x_rec)}")
+        logger.info(f"SSIM {solver_name}: {ssim(x=x, x_hat=x_rec)}")
 
     ModelPaths.VAE_LATEST_PLOTS.mkdir(exist_ok=True)
     plt.savefig(ModelPaths.VAE_LATEST_PLOTS / "inverse_reconstruction.png")
