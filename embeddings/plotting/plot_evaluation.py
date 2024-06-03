bfe91c2b1f3c5e07842780b8170a4da63dc1e7d3
must1d <mustafe.dobra@tum.de>
2024-06-03 07:49:15 +0000

    Adds Axis Limits to Evaluation Plot

--- embeddings/plotting/plot_evaluation.py
+++ embeddings/plotting/plot_evaluation.py
@@ -37,11 +37,13 @@ if __name__ == "__main__":
     ax1.set_xscale("log")
     ax1.set_xticks(ticks=measurements, labels=[str(m) for m in measurements], rotation=90)
     ax1.set_xlim([measurements[0], measurements[-1]])
+    ax1.set_ylim([0.07, 0.4])
     ax1.errorbar(x=measurements, y=mean_mse, yerr=confidence_mse)
 
     ax2.set_xscale("log")
     ax2.set_xticks(ticks=measurements, labels=[str(m) for m in measurements], rotation=90)
     ax2.set_xlim([measurements[0], measurements[-1]])
+    ax2.set_ylim([0.78, 0.87])
     ax2.errorbar(x=measurements, y=mean_ssim, yerr=confidence_ssim)
 
     ModelPaths.VAE_LATEST_PLOTS.mkdir(exist_ok=True)
