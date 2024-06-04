898a357f4e8faaa1a77fa254e1c09842b0a365eb
must1d <mustafe.dobra@tum.de>
2024-06-04 17:38:00 +0000

    Adds Sectors to Inverse Problem Script

--- embeddings/plotting/plot_inverse_problem_reconstruction.py
+++ embeddings/plotting/plot_inverse_problem_reconstruction.py
@@ -2,14 +2,36 @@ import random
 
 import torch
 from matplotlib import pyplot as plt
+from matplotlib.axis import Axis
+from torch import Tensor
 
+from embeddings.common.gnfr_sector import GnfrSector
 from embeddings.common.log import logger
-from embeddings.common.paths import ModelPaths
+from embeddings.common.paths import ModelPaths, PlotPaths
 from embeddings.dataset.tno_dataset_collection import TnoDatasetCollection
 from embeddings.evaluation.compressed_sensing_experiment import generate_random_inverse_problem, solve_inverse_problem
-from embeddings.evaluation.inverse_problems_solver import DwtLassoSolver, GenerativeModelSolver, LassoSolver
+from embeddings.evaluation.inverse_problems_solver import (
+    DctLassoSolver,
+    DwtLassoSolver,
+    GenerativeModelSolver,
+    LassoSolver,
+)
 from embeddings.models.common.metrics import mse, ssim
 from embeddings.plotting.city_emission_field_plot import plot_emission_field_tensor
+
+SECTORS_TO_PLOT = [GnfrSector.B, GnfrSector.C, GnfrSector.F2]
+
+
+def plot_reconstruction(axes_: tuple[tuple[Axis]], field: Tensor, vmax_: float, title: str, index_: int) -> None:
+    ax = axes_[0][index_]
+    plot_emission_field_tensor(emission_field=field, ax=ax, vmax=vmax_)
+    ax.title.set_text(title)
+
+    for i, sector in enumerate(SECTORS_TO_PLOT):
+        ax = axes_[i + 1][index_]
+        plot_emission_field_tensor(emission_field=field, ax=ax, vmax=vmax_, sector=sector)
+        ax.title.set_text(f"{sector}")
+
 
 if __name__ == "__main__":
     dataset = TnoDatasetCollection().test_data
@@ -18,28 +40,43 @@ if __name__ == "__main__":
     num_measurements = 500
 
     solvers = {
-        "Generative AI": GenerativeModelSolver(plot_loss=True, log_info=True),
+        "VAE 128": GenerativeModelSolver(plot_loss=True, log_info=True, path_to_model=ModelPaths.get_vae_model("128")),
+        "VAE 256": GenerativeModelSolver(path_to_model=ModelPaths.get_vae_model("256")),
         "Lasso": LassoSolver(),
-        "Lasso + DWT": DwtLassoSolver(),
+        "Lasso (DWT)": DwtLassoSolver(),
+        "Lasso (DCT)": DctLassoSolver(),
     }
 
     x = dataset[random.randint(0, len(dataset) - 1)]
     inverse_problem = generate_random_inverse_problem(x=x, num_measurements=num_measurements)
 
-    fig, axes = plt.subplots(1, len(solvers) + 1, figsize=(5 + 5 * len(solvers), 8))
+    fig, axes = plt.subplots(
+        len(SECTORS_TO_PLOT) + 1,
+        len(solvers) + 1,
+        figsize=(5 + 5 * len(solvers), 5 + 5 * len(SECTORS_TO_PLOT)),
+    )
     vmax = 1.1 * float(torch.max(x))
-    plot_emission_field_tensor(emission_field=x, ax=axes[0], vmax=vmax)
-    axes[0].title.set_text("Original Emission Field")
+
+    plot_reconstruction(
+        axes_=axes,
+        field=x,
+        index_=0,
+        vmax_=vmax,
+        title="Original Emission Field",
+    )
 
     for index, (solver_name, solver) in enumerate(solvers.items()):
         x_rec = solve_inverse_problem(solver=solver, inverse_problem=inverse_problem)
 
-        ax = axes[index + 1]
-        plot_emission_field_tensor(emission_field=x_rec, ax=ax, vmax=vmax)
-        ax.title.set_text(f"Reconstructed Emission Field\nWith {num_measurements} measurements\n{solver_name}")
+        plot_reconstruction(
+            axes_=axes,
+            field=x_rec,
+            index_=index + 1,
+            vmax_=vmax,
+            title=f"Reconstructed Emission Field\nWith {num_measurements} measurements\n{solver_name}",
+        )
 
         logger.info(f"MSE {solver_name}: {mse(x=x, x_hat=x_rec)}")
         logger.info(f"SSIM {solver_name}: {ssim(x=x, x_hat=x_rec)}")
 
-    ModelPaths.VAE_LATEST_PLOTS.mkdir(exist_ok=True)
-    plt.savefig(ModelPaths.VAE_LATEST_PLOTS / "inverse_reconstruction.png")
+    plt.savefig(PlotPaths.PLOTS / "inverse_reconstruction.png")
