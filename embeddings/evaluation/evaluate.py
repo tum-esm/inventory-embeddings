e7ebcd17a252ebb995049a786f11d53a5b8ffa4f
must1d <mustafe.dobra@tum.de>
2024-06-04 08:32:30 +0000

    Extends Evaluation to Multiple Solvers

--- embeddings/evaluation/evaluate.py
+++ embeddings/evaluation/evaluate.py
@@ -1,13 +1,11 @@
-import shutil
-
 import numpy as np
 from tqdm import tqdm
 
 from embeddings.common.csv_writer import CsvWriter
-from embeddings.common.paths import ModelPaths
+from embeddings.common.paths import ExperimentPaths
 from embeddings.dataset.tno_dataset_collection import TnoDatasetCollection
 from embeddings.evaluation.compressed_sensing_experiment import generate_random_inverse_problem, solve_inverse_problem
-from embeddings.evaluation.inverse_problems_solver import GenerativeModelSolver
+from embeddings.evaluation.inverse_problems_solver import DwtLassoSolver, GenerativeModelSolver, LassoSolver
 from embeddings.models.common.metrics import ssim
 
 NUM_MEASUREMENTS = [50, 100, 250, 500, 1000, 2500, 5000, 10000, 12500]
@@ -18,22 +16,25 @@ def evaluate() -> None:
     dataset = TnoDatasetCollection().test_data
     dataset.disable_temporal_transforms()
 
-    generative_model_solver = GenerativeModelSolver()
+    solvers = {
+        "Generative": GenerativeModelSolver(),
+        "Lasso": LassoSolver(),
+        "Lasso_DWT": DwtLassoSolver(),
+    }
 
-    if ModelPaths.VAE_LATEST_EVALUATION.exists():
-        shutil.rmtree(ModelPaths.VAE_LATEST_EVALUATION)
-    ModelPaths.VAE_LATEST_EVALUATION.mkdir()
+    ExperimentPaths.archive_latest_evaluation()
 
     for run_index in tqdm(range(NUM_EXPERIMENTS), desc="Run"):
-        evaluation_csv = CsvWriter(path=ModelPaths.VAE_LATEST_EVALUATION / f"evaluation_{run_index}.csv")
-        evaluation_csv.write_header("Measurements", "MSE", "SSIM")
+        evaluation_csv = CsvWriter(path=ExperimentPaths.EVALUATION_LATEST / f"evaluation_{run_index}.csv")
+        evaluation_csv.write_header("Measurements", "Solver", "MSE", "SSIM")
 
         with tqdm(total=len(NUM_MEASUREMENTS) * len(dataset), leave=False, desc="Compressed Sensing") as bar:
             for num_measurements in NUM_MEASUREMENTS:
                 for x in dataset:
                     inverse_problem = generate_random_inverse_problem(x=x, num_measurements=num_measurements)
-                    x_rec = solve_inverse_problem(solver=generative_model_solver, inverse_problem=inverse_problem)
-                    mse = float(np.square(np.subtract(x, x_rec)).mean())
-                    s = float(ssim(x=x, x_hat=x_rec))
-                    evaluation_csv.write_row(num_measurements, mse, s)
+                    for name, solver in solvers.items():
+                        x_rec = solve_inverse_problem(solver=solver, inverse_problem=inverse_problem)
+                        mse = float(np.square(np.subtract(x, x_rec)).mean())
+                        s = float(ssim(x=x, x_hat=x_rec))
+                        evaluation_csv.write_row(num_measurements, name, mse, s)
                     bar.update()
--- embeddings/plotting/plot_evaluation.py
+++ embeddings/plotting/plot_evaluation.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
 
-from embeddings.common.paths import ModelPaths
+from embeddings.common.paths import ExperimentPaths
 
 
-def _get_mean_and_confidence_tuples(metric: str) -> dict[int, tuple[float, float]]:
+def _get_mean_and_confidence_tuples(pl_values: pl.DataFrame, metric: str) -> dict[int, tuple[float, float]]:
     values_dict = {}
-    for num_measurements, group in evaluation.group_by(["Measurements"]):
+    for num_measurements, group in pl_values.group_by(["Measurements"]):
         values = group.get_column(metric).to_numpy()
         mean = np.mean(values)
         std_error = np.std(values, ddof=1) / np.sqrt(len(values))
@@ -18,33 +18,34 @@ def _get_mean_and_confidence_tuples(metric: str) -> dict[int, tuple[float, float
 
 
 if __name__ == "__main__":
-    data_frames = [pl.read_csv(evaluation_csv) for evaluation_csv in ModelPaths.VAE_LATEST_EVALUATION.iterdir()]
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25, 10))
+
+    data_frames = [pl.read_csv(evaluation_csv) for evaluation_csv in ExperimentPaths.EVALUATION_LATEST.glob("*.csv")]
     evaluation = pl.concat(data_frames)
 
-    mse_values = _get_mean_and_confidence_tuples(metric="MSE")
-    ssim_values = _get_mean_and_confidence_tuples(metric="SSIM")
+    for solver, group in evaluation.group_by("Solver"):
+        mse_values = _get_mean_and_confidence_tuples(pl_values=group, metric="MSE")
+        ssim_values = _get_mean_and_confidence_tuples(pl_values=group, metric="SSIM")
 
-    measurements = list(mse_values.keys())
+        measurements = list(mse_values.keys())
 
-    mean_mse = [value[0] for value in mse_values.values()]
-    confidence_mse = [value[1] / 2 for value in mse_values.values()]
+        mean_mse = [value[0] for value in mse_values.values()]
+        confidence_mse = [value[1] / 2 for value in mse_values.values()]
 
-    mean_ssim = [value[0] for value in ssim_values.values()]
-    confidence_ssim = [value[1] / 2 for value in ssim_values.values()]
+        mean_ssim = [value[0] for value in ssim_values.values()]
+        confidence_ssim = [value[1] / 2 for value in ssim_values.values()]
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25, 10))
+        ax1.errorbar(x=measurements, y=mean_mse, yerr=confidence_mse, label=solver)
+        ax2.errorbar(x=measurements, y=mean_ssim, yerr=confidence_ssim, label=solver)
 
     ax1.set_xscale("log")
     ax1.set_xticks(ticks=measurements, labels=[str(m) for m in measurements], rotation=90)
     ax1.set_xlim([measurements[0], measurements[-1]])
-    ax1.set_ylim([0.07, 0.4])
-    ax1.errorbar(x=measurements, y=mean_mse, yerr=confidence_mse)
+    ax1.legend()
 
     ax2.set_xscale("log")
     ax2.set_xticks(ticks=measurements, labels=[str(m) for m in measurements], rotation=90)
     ax2.set_xlim([measurements[0], measurements[-1]])
-    ax2.set_ylim([0.78, 0.87])
-    ax2.errorbar(x=measurements, y=mean_ssim, yerr=confidence_ssim)
+    ax2.legend()
 
-    ModelPaths.VAE_LATEST_PLOTS.mkdir(exist_ok=True)
-    plt.savefig(ModelPaths.VAE_LATEST_PLOTS / "evaluation.png")
+    plt.savefig(ExperimentPaths.EVALUATION_LATEST / "evaluation.png")
