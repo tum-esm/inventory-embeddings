6d6927d964fa274098d33645ff1aace7ecac32f6
must1d <mustafedobra@outlook.de>
2024-07-23 13:51:01 +0200

    Adjusts Metric to Many Different Dimensions

--- embeddings/models/common/metrics.py
+++ embeddings/models/common/metrics.py
@@ -3,43 +3,62 @@ import torch
 from torch import Tensor
 from torchmetrics.image import StructuralSimilarityIndexMeasure
 
+_TWO = 2
+_THREE = 3
+
+_CHANNEL_ERROR_MESSAGE = "Cannot provide channel if number of dimensions is only 2!"
+
 _SSIM = StructuralSimilarityIndexMeasure()
 
-
 def ssim(x: Tensor, x_hat: Tensor, channel: int | None = None) -> Tensor:
-    three_dimensions = 3
+    if x.ndimension() == _TWO:
+        if channel is not None:
+            raise AttributeError(_CHANNEL_ERROR_MESSAGE)
+        x = x.unsqueeze(0).unsqueeze(0)
+        x_hat = x_hat.unsqueeze(0).unsqueeze(0)
+    elif x.ndimension() == _THREE:
+        x = x.unsqueeze(0)
+        x_hat = x_hat.unsqueeze(0)
+    if channel:
+        x = x[:, channel : channel + 1, :, :]
+        x_hat = x_hat[:, channel : channel + 1, :, :]
 
     if _SSIM.device != x.device:
         _SSIM.to(x.device)
-    if x.dim() == three_dimensions:
-        x = x.unsqueeze(0)
-    if x_hat.dim() == three_dimensions:
-        x_hat = x_hat.unsqueeze(0)
-    if channel:
-        return _SSIM(x[:, channel : channel + 1, :, :], x_hat[:, channel : channel + 1, :, :])
     return _SSIM(x, x_hat)
 
 
 def mse(x: Tensor, x_hat: Tensor, channel: int | None = None) -> Tensor:
+    if x.ndimension() == _TWO:
+        if channel is not None:
+            raise AttributeError(_CHANNEL_ERROR_MESSAGE)
+        x = x.unsqueeze(0).unsqueeze(0)
+        x_hat = x_hat.unsqueeze(0).unsqueeze(0)
+    elif x.ndimension() == _THREE:
+        x = x.unsqueeze(0)
+        x_hat = x_hat.unsqueeze(0)
+    if channel:
+        x = x[:, channel : channel + 1, :, :]
+        x_hat = x_hat[:, channel : channel + 1, :, :]
+
     metric = torch.nn.MSELoss(reduction="mean")
     if channel is not None:
         return metric(x[:, channel : channel + 1, :, :], x_hat[:, channel : channel + 1, :, :])
     return metric(x, x_hat)
 
 def relative_error(x: Tensor, x_hat: Tensor, channel: int | None = None) -> Tensor:
-    three_dimensions = 3
-
-    if x.dim() != three_dimensions:
-        x = x.squeeze(0)
-    if x_hat.dim() != three_dimensions:
-        x_hat = x_hat.squeeze(0)
+    if x.ndimension() == _TWO:
+        if channel is not None:
+            raise AttributeError(_CHANNEL_ERROR_MESSAGE)
+    elif x.ndimension() == _THREE:
+        x = x.unsqueeze(0)
+        x_hat = x_hat.unsqueeze(0)
+    if channel:
+        x = x[:, channel : channel + 1, :, :]
+        x_hat = x_hat[:, channel : channel + 1, :, :]
 
     x_np = np.array(x)
     x_hat_np = np.array(x_hat)
-
-    if channel is not None:
-        x_np = x_np[:, channel : channel + 1, :, :]
-        x_hat_np = x_hat_np[:, channel : channel + 1, :, :]
 
     p = 2
 
