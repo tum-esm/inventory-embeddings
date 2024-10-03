1f6fbc1a905ea0e151e8fc26622c54f380e51197
must1d <mustafedobra@outlook.de>
2024-10-03 07:20:47 +0000

    Converts Experiments to Python Scripts

---
+++ src/experiments/gen_models_snr_20_db.py
@@ -0,0 +1,38 @@
+from src.common.paths import ExperimentPath
+from src.dataset.tno_dataset_collection import TnoDatasetCollection
+from src.experiments.evaluation.evaluation_runner import EvaluationRunner, EvaluationSettings
+from src.inverse_problems.inverse_problems_solver import GenerativeModelSolver, InverseProblemSolver
+
+EXPERIMENT_NAME = "gen_models_snr_20_db"
+
+MODEL_256 = "256"
+MODEL_512 = "512"
+MODEL_1024 = "1024"
+MODEL_2048 = "2048"
+
+SNR = 100  # 20 dB
+NUM_MEASUREMENTS = [500, 1000, 2500, 5000, 10000, 12500]
+
+dataset_collection = TnoDatasetCollection()
+
+dataset = dataset_collection.test_data
+
+dataset.disable_temporal_transforms()
+
+solvers: dict[str, InverseProblemSolver] = {
+    MODEL_256: GenerativeModelSolver.from_vae_model_name(MODEL_256),
+    MODEL_512: GenerativeModelSolver.from_vae_model_name(MODEL_512),
+    MODEL_1024: GenerativeModelSolver.from_vae_model_name(MODEL_1024),
+    MODEL_2048: GenerativeModelSolver.from_vae_model_name(MODEL_2048),
+}
+
+settings = EvaluationSettings(
+    measurements=NUM_MEASUREMENTS,
+    snr=[SNR],
+    dataset=dataset,
+    path=ExperimentPath(EXPERIMENT_NAME),
+)
+
+evaluation_runner = EvaluationRunner(settings=settings)
+
+evaluation_runner.run(solvers=solvers)
---
+++ src/experiments/lasso_snr_20_db.py
@@ -0,0 +1,32 @@
+from src.common.paths import ExperimentPath
+from src.dataset.tno_dataset_collection import TnoDatasetCollection
+from src.experiments.evaluation.evaluation_runner import EvaluationRunner, EvaluationSettings
+from src.inverse_problems.inverse_problems_solver import InverseProblemSolver, LassoSolver, SparsityTransform
+
+EXPERIMENT_NAME = "lasso_snr_20_db"
+
+SNR = 100  # 20 dB
+NUM_MEASUREMENTS = [500, 1000, 2500, 5000, 10000, 12500]
+
+dataset_collection = TnoDatasetCollection()
+
+dataset = dataset_collection.test_data
+
+dataset.disable_temporal_transforms()
+
+solvers: dict[str, InverseProblemSolver] = {
+    "lasso": LassoSolver(),
+    "lasso_dwt": LassoSolver(transform=SparsityTransform.DWT),
+    "lasso_dct": LassoSolver(transform=SparsityTransform.DCT),
+}
+
+settings = EvaluationSettings(
+    measurements=NUM_MEASUREMENTS,
+    snr=[SNR],
+    dataset=dataset,
+    path=ExperimentPath(EXPERIMENT_NAME),
+)
+
+evaluation_runner = EvaluationRunner(settings=settings)
+
+evaluation_runner.run(solvers=solvers)
