65a33d24e23709829f65876e98d978a7bc6fea1d
must1d <mustafedobra@outlook.de>
2024-10-03 08:13:22 +0000

    Fixes Sector Uncertainty Transform

--- src/dataset/emission_field_transforms/_sector_uncertainty_transform.py
+++ src/dataset/emission_field_transforms/_sector_uncertainty_transform.py
@@ -9,7 +9,7 @@ from . import EmissionFieldTransform
 class SectorUncertaintyTransform(EmissionFieldTransform):
     def __call__(self, emission_field: CityEmissionField) -> CityEmissionField:
         generator = np.random.default_rng()
-        scaling_factor = generator.uniform(0.5, 1.5, NUM_GNFR_SECTORS)
+        scaling_factor = generator.uniform(0.5, 1.5, (NUM_GNFR_SECTORS, 1, 1))
         emission_field.co2_ff_area_sources_field *= scaling_factor
         emission_field.co2_ff_point_sources_field *= scaling_factor
         return emission_field
