99606b314b2e183f74c4b425b55b95213128d636
must1d <mustafedobra@outlook.de>
2024-10-11 10:17:36 +0200

    Aligns Plots With Settings

---
+++ src/plotting/plot_settings.py
@@ -0,0 +1,13 @@
+PLOT_WIDTH = 6
+PLOT_HEIGHT = 6
+
+LABEL_FONT_SIZE = 14
+LEGEND_FONT_SIZE = 12
+
+GRID_LINE_WIDTH = 0.75
+
+PLOT_LINE_OPTIONS = {
+    "linestyle": "--",
+    "markersize": 10,
+    "linewidth": 2,
+}
